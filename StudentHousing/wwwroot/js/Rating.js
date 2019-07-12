71e39467845bc9072b560306e223a868932500c1
LeonMacovski <leonmacovski@gmail.com>
2019-07-12 14:22:01 +0200

    Rating Stars

--- StudentHousing/wwwroot/js/Rating.js
+++ StudentHousing/wwwroot/js/Rating.js
@@ -1,23 +1,6 @@
 ﻿//var myBtn = document.getElementById('myButton');
 //myBtn.addEventListener('click', function (event) {
 //    window.location.href = 'Index.cshtml';
-function Rate1() {
-    alert("Rated 1 star");
-}
-
-function Rate2() {
-    alert("Rated 2 stars");
-}
-
-function Rate3() {
-    alert("Rated 3 stars");
-}
-
-function Rate4() {
-    alert("Rated 4 stars");
-}
-
-
-function Rate5() {
-    alert("Rated 5 stars");
+function getRating(a) {
+    alert(a);
 }
