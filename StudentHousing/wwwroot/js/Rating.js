23cbe271196a92de689bb1edd99df0644dcf1f73
LeonMacovski <leonmacovski@gmail.com>
2019-07-15 10:38:49 +0200

    Rate Testing

--- StudentHousing/wwwroot/js/Rating.js
+++ StudentHousing/wwwroot/js/Rating.js
@@ -1,7 +1,4 @@
-﻿//var myBtn = document.getElementById('myButton');
-//myBtn.addEventListener('click', function (event) {
-//    window.location.href = 'Index.cshtml';
-$(document).ready(function getRating(a) {
+﻿$(document).ready(function getRating(a) {
 
     //var apartmentId = 1;
     //var data = { "ApartmentId": apartmentId, "RatingValue": a };
@@ -21,33 +18,21 @@ $(document).ready(function getRating(a) {
     event.preventDefault();
 
     var apartmentId = 1;
-   // var data = { "ApartmentId": apartmentId, "RatingValue": a };
+    var data = { "ApartmentId": apartmentId, "RatingValue": a };
 
-    var ApartmentId = $(this).find('#Rating_ApartmentId');
-    var RatingValue = $(this).find('#Rating_RatingValue');
-})
+
 
     $.ajax({
         method: 'POST',
         url: 'home/addrating',
-        data: JSON.stringify(model),
+        data: JSON.stringify(gdata),
         contentType: "application/json",
         success: function (data) {
-
+            console.log("Success");
         },
         error: function (error) {
             console.log('An error accured!')
             console.log(error);
         }
     })
-
-
-   // alert(a);
-    //$.post("demo_test_post.asp",
-    //    {
-    //        name: "Donald Duck",
-    //        city: "Duckburg"
-    //    },
-    //    function (data, status) {
-    //        alert("Data: " + data + "\nStatus: " + status);
-    //    });
+})
