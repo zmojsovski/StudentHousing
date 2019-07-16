24fbc85f4f6e3538182959074bc7eb3308c1d68a
LeonMacovski <leonmacovski@gmail.com>
2019-07-16 16:31:00 +0200

    Rating Hover Over

--- StudentHousing/wwwroot/js/Rating.js
+++ StudentHousing/wwwroot/js/Rating.js
@@ -1,48 +1,25 @@
 ﻿$(document).ready(function () {
+    $(".rating").hover(function () {
+        event.preventDefault();
+        var VAL = $(this).attr("value");
+        var Parent = $(this).parent().parent();
+        for (let i = 0; i <= VAL; i++) {
+            Parent.children(":nth-of-type(" + i + ")").children().css("color", "yellow");
+        }
+        //alert(VAL);
 
-    //$(".rating").hover(function () {
-    //    rated = false;
-
-    //    var id = $(this).parent().parent().parent().parent().children(":nth-of-type(2)").children().attr("value");
-
-    //    if (this.id == "1") {
-    //        $(this).css("color", "yellow");
-    //    }
-    //    else if (this.id == "2") {
-    //        $("#1").css("color", "yellow");
-    //        $(this).css("color", "yellow");
-    //    }
-    //    else if (this.id == "3") {
-    //        $("#1").css("color", "yellow");
-    //        $("#2").css("color", "yellow");
-    //        $(this).css("color", "yellow");
-    //    }
-    //    else if (this.id == "4") {
-    //        $("#1").css("color", "yellow");
-    //        $("#2").css("color", "yellow");
-    //        $("#3").css("color", "yellow");
-    //        $(this).css("color", "yellow");
-    //    }
-    //    else if (this.id == "5") {
-    //        $("#1").css("color", "yellow");
-    //        $("#2").css("color", "yellow");
-    //        $("#3").css("color", "yellow");
-    //        $("#4").css("color", "yellow");
-    //        $(this).css("color", "yellow");
-    //    }
-    //},
-    //    function () {
-    //        if (rated == false) {
-    //            $("#1").css("color", "grey");
-    //            $("#2").css("color", "grey");
-    //            $("#3").css("color", "grey");
-    //            $("#4").css("color", "grey");
-    //            $("#5").css("color", "grey");
-    //        }
-    //    });
+    },
+        function () {
+            var Parent = $(this).parent().parent();
+            var VAL = $(this).attr("value");
+            for (let i = 0; i <= VAL; i++) {
+                Parent.children(":nth-of-type(" + i + ")").children().css("color", "grey");
+            }
+        });
 
     $(".rating").click(function () {
         event.preventDefault();
+
         var apartmentId = $(this).parent().parent().parent().parent().children(":nth-of-type(2)").children().attr("value");
         var ratingValueText = $(this).parent().parent().parent().parent().children(":nth-of-type(7)").children();
        // alert(ratingValueText);
