214731ea2888c505879719f6cbb4015ea48af79a
Zane Mojsovski <zankomojsoski@gmail.com>
2019-07-22 13:49:04 +0200

    Apartment Creation( Are you sure dialog)

--- StudentHousing/wwwroot/js/CreateApt.js
+++ StudentHousing/wwwroot/js/CreateApt.js
@@ -3,8 +3,13 @@
         dateFormat: "mm-dd-yy",
         minDate: -0,
     });
-    $("#apartmentcreate").click(function () {
-        confirm("Are you ready to submit?");
-        document.getElementsByClassName(".form-inline").reset();
+    
+});
+
+$("#apartmentcreate").click(function () {
+    //confirm("Are you ready to submit?");
+    //document.getElementsByClassName(".form-inline").reset();
+    $("body").on("submit", "#Form1", function () {
+        return confirm("Are you sure you want to create the apartment?");
     });
 });
